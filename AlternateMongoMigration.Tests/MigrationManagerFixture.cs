03e94f8018e8c1f4d4a35a2b86957d338150c98e
Markus Grigull <web@grigull.me>
2018-05-27 23:04:46 +0200

    Add first unit tests to migration manager

--- AlternateMongoMigration.Tests/MigrationManagerFixture.cs
+++ AlternateMongoMigration.Tests/MigrationManagerFixture.cs
@@ -1,10 +1,33 @@
-﻿using NUnit.Framework;
+﻿using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
 
 namespace AlternateMongoMigration.Tests
 {
     [TestFixture]
     public class MigrationManagerFixture
     {
+        [Test]
+        public void DefaultProperties()
+        {
+            var migrationManager = new MigrationManager();
 
+            Assert.AreEqual(migrationManager.MigrationDatabaseName, "general");
+            Assert.AreEqual(migrationManager.MigrationDatabaseCollection, "migration");
+            Assert.AreEqual(migrationManager.Batch, 0);
+        }
+
+        [Test]
+        public void ConnectWithDefaultConfigurations()
+        {
+            var migrationManager = new MigrationManager();
+
+            // add general database
+            var generalDatabase = new Mock<IMongoDatabase>();
+
+            migrationManager.AddDatabase(generalDatabase.Object, "general");
+
+            Assert.AreEqual(migrationManager.GetDatabase("general"), generalDatabase.Object);
+        }
     }
 }
