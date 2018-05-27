55d019026bb8f298704bf37135548d0ff5483566
Markus Grigull <web@grigull.me>
2018-05-27 20:43:05 +0200

    Add migration loading and client handling

--- AlternateMongoMigration/Interfaces/IMigrationManager.cs
+++ AlternateMongoMigration/Interfaces/IMigrationManager.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
+using MongoDB.Driver;
 
 namespace AlternateMongoMigration.Interfaces
 {
     public interface IMigrationManager
     {
+        void AddClientForDatabase(MongoClient client, string databaseName);
+
+        MongoClient GetClientForDatabase(string databaseName);
+
+        void LoadMigrations();
+
+        void LoadMigration(IEnumerable<Assembly> assemblies);
+
         IEnumerable<IMigration> GetMigrations();
 
         IEnumerable<IMigration> GetUnappliedMigrations();
 
         IEnumerable<IMigration> GetAppliedMigrations();
+
+        bool IsMigrationApplied(string name);
+
+        bool IsMigrationApplied(IMigration migration);
     }
 }
--- AlternateMongoMigration/MigrationManager.cs
+++ AlternateMongoMigration/MigrationManager.cs
@@ -1,15 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using AlternateMongoMigration.Interfaces;
+using MongoDB.Driver;
 
 namespace AlternateMongoMigration
 {
     public class MigrationManager : IMigrationManager
     {
         private readonly List<IMigration> _migrations;
+        private readonly Dictionary<string, MongoClient> _clients;
 
         public MigrationManager()
         {
             _migrations = new List<IMigration>();
+            _clients = new Dictionary<string, MongoClient>();
+        }
+
+        public void AddClientForDatabase(MongoClient client, string databaseName)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName));
+            }
+
+            _clients[databaseName] = client;
+        }
+
+        public MongoClient GetClientForDatabase(string databaseName)
+        {
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName));
+            }
+
+            return _clients.ContainsKey(databaseName) ? _clients[databaseName] : null;
+        }
+
+        public void LoadMigrations()
+        {
+            LoadMigration(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+        public void LoadMigration(IEnumerable<Assembly> assemblies)
+        {
+            // get all migration type infos
+            var migrationTypes = new List<TypeInfo>();
+
+            foreach (var assembly in assemblies)
+            {
+                migrationTypes.AddRange(assembly.DefinedTypes.Where(t => t.ImplementedInterfaces.Contains(typeof(IMigration))).OrderBy(m => m.Name));
+            }
+
+            foreach (var typeInfo in migrationTypes)
+            {
+                var migration = (IMigration) Activator.CreateInstance(typeInfo.AsType());
+
+                _migrations.Add(migration);
+            }
         }
 
         public IEnumerable<IMigration> GetMigrations()
@@ -19,12 +73,22 @@ namespace AlternateMongoMigration
 
         public IEnumerable<IMigration> GetUnappliedMigrations()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public IEnumerable<IMigration> GetAppliedMigrations()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        public bool IsMigrationApplied(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMigrationApplied(IMigration migration)
+        {
+            return IsMigrationApplied(migration.GetType().Name);
         }
     }
 }
